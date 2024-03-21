2a93c72cbab630f4801d27633b6a41c1fade4308
Airton Gabriel <55886112+airtong@users.noreply.github.com>
2024-03-20 21:55:48 -0300

    [fix] update strangeStuffStoreTest class name

--- strange_stuff_store/src/test/java/strangeStuffStoreTest.java
+++ strange_stuff_store/src/test/java/strangeStuffStoreTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class strageStuffStoreTest {
+public class strangeStuffStoreTest {
 
     Checkout checkout;
     UrbanRaccoon raccoon1;
