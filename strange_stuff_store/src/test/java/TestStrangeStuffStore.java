7bfa3a054cd4fd98cf2b022ef88ff1a2c0f923f0
lrgsouza <lucas_souza@outlook.com>
2024-03-27 19:52:47 -0300

    Change test class name + fix results name

--- strange_stuff_store/src/test/java/TestStrangeStuffStore.java
+++ strange_stuff_store/src/test/java/TestStrangeStuffStore.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class strangeStuffStoreTest {
+public class TestStrangeStuffStore {
 
     Checkout checkout;
     UrbanRaccoon raccoon1;
